6f65f71d529d99c68badc01f82aeed01161a489c
grims-dev <ciarangrimshawdev@gmail.com>
2023-08-03 00:36:40 +0100

    update interaction type

--- src/commands/test/ping.ts
+++ src/commands/test/ping.ts
@@ -1,11 +1,11 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { ICommand } from '../../types';
 
 const command: ICommand = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction: any) {
+	async execute(interaction: CommandInteraction) {
 		await interaction.reply('Pong!');
 	},
 };
