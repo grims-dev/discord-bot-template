d06768a4ca02c76c99084078c495e1f444925562
grims-dev <ciarangrimshawdev@gmail.com>
2023-08-03 00:10:05 +0100

    deploy commands to discord api

---
+++ src/requests/deploy-commands.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import { REST, Routes } from 'discord.js';
+import { getCommandsJSON } from '../utils';
+
+async function deployCommands() {
+    const {
+        DISCORD_BOT_TOKEN: botToken,
+        DISCORD_CLIENT_ID: clientId,
+        DISCORD_GUILD_ID: guildId,
+        DEPLOY_OR_DELETE: deployOrDelete,
+        GLOBAL_OR_GUILD: globalOrGuild,
+    } = process.env;
+
+    if (!botToken || !clientId || !guildId || !deployOrDelete || !globalOrGuild) {
+        console.error('Invalid env var setup');
+        return;
+    }
+
+    const rest = new REST().setToken(botToken);
+
+    const route = globalOrGuild === 'GLOBAL'
+        ? Routes.applicationCommands(clientId)
+        : Routes.applicationGuildCommands(clientId, guildId);
+
+    const body = deployOrDelete === 'DEPLOY'
+        ? getCommandsJSON()
+        : [];
+
+    await rest.put(route, { body });
+}
+
+deployCommands();
